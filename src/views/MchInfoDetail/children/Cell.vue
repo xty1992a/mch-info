96ccb98c27f57c08d0c02005201de7e974567dfb
谢腾远 <redbuck@126.com>
2019-07-11 11:55:14 +0800

    refect: 调整详情页面样式

--- src/views/MchInfoDetail/children/Cell.vue
+++ src/views/MchInfoDetail/children/Cell.vue
@@ -1,57 +1,56 @@
 <script>
-  import { Cell } from "vant";
+import { Cell } from "vant";
 
-  export default {
-    name: "Cell",
-    functional: true,
-    render(h, context) {
-      return context.props.isMobile ? (
-        <Cell {...context} class="van-hairline--bottom"/>
-      ) : (
-        <div class="ui-cell">
-          <div class="title">
-            <slot><span title={context.props.title}>{context.props.title}</span></slot>
-          </div>
-          <div class="value">
-            <slot><span>{context.props.value}</span></slot>
-          </div>
+export default {
+  name: "Cell",
+  functional: true,
+  render(h, context) {
+    return context.props.isMobile ? (
+      <Cell {...context} class="van-hairline--bottom" />
+    ) : (
+      <div class="ui-cell">
+        <div class="title">
+          <span title={context.props.title}>{context.props.title}</span>
         </div>
-      );
-    }
-  };
+        <div class="value">
+          <span>{context.props.value}</span>
+        </div>
+      </div>
+    );
+  }
+};
 </script>
 
 <style lang="less" rel="stylesheet/less">
-  @import "../../../styles/variable";
+@import "../../../styles/variable";
 
-  .ui-cell {
-    padding: 10px;
-    display: flex;
-    align-items: center;
+.ui-cell {
+  padding: 10px;
+  display: flex;
+  align-items: center;
 
-    .title, .value {
-      display: inline-block;
-      vertical-align: middle;
-    }
-
-    .title {
-      .two-line;
-      color: #999;
-      justify-content: flex-end;
-      width: 30%;
-      min-width: 100px;
-      text-align: right;
-      margin-right: 10px;
-      padding-right: 10px;
-      position: relative;
-
-      &:after {
-        content: ':';
-        position: absolute;
-        right: 0;
-        top: 0;
-      }
-    }
-
+  .title,
+  .value {
+    display: inline-block;
+    vertical-align: middle;
   }
+
+  .title {
+    line-height: 1.3;
+    color: #999;
+    width: 30%;
+    min-width: 100px;
+    text-align: right;
+    margin-right: 10px;
+    padding-right: 10px;
+    position: relative;
+
+    &:after {
+      content: ":";
+      position: absolute;
+      right: 0;
+      top: 0;
+    }
+  }
+}
 </style>
