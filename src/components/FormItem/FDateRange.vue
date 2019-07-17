bb42fdc31c1ecfaac7304f4cfe7f3bf14b89e9cd
谢腾远 <redbuck@126.com>
2019-07-17 10:16:42 +0800

    refect: 将时间选择放开到-35~+100,修复了移动端结束时间设置永久有效时,选择开始时间非常卡的问题

--- src/components/FormItem/FDateRange.vue
+++ src/components/FormItem/FDateRange.vue
@@ -1,247 +1,274 @@
 <template>
   <div class="f-date-range f-form-item">
-    <div class="pick-holder" :class="{forever}" v-if="isMobile">
+    <div class="pick-holder" :class="{ forever }" v-if="isMobile">
       <span class="start-time time-display" @click="callTimePicker('start')">
         <span v-if="startTime">{{ startTime | fmt }}</span>
         <span class="placeholder" v-else>开始日期</span>
       </span>
       <span style="padding: 0 4px;">至</span>
-      <el-switch v-model="isForever" v-if="forever"
-                 active-text="永久有效"
-                 inactive-text=""/>
-      <span class="end-time time-display" :class="isForever?'disabled':''" @click="callTimePicker('end')">
+      <el-switch
+        v-model="isForever"
+        v-if="forever"
+        active-text="永久有效"
+        inactive-text=""
+      />
+      <span
+        class="end-time time-display"
+        :class="isForever ? 'disabled' : ''"
+        @click="callTimePicker('end')"
+      >
         <span v-if="endTime">{{ endTime | fmt }}</span>
         <span class="placeholder" v-else>结束日期</span>
       </span>
     </div>
     <template v-else>
       <el-date-picker
-              v-model="startTime"
-              :picker-options="startOptions"
-              placeholder="开始日期"
+        v-model="startTime"
+        :picker-options="startOptions"
+        placeholder="开始日期"
       />
       <span style="padding: 0 4px;">至</span>
       <el-date-picker
-              :disabled="isForever"
-              v-model="endTime"
-              :picker-options="endOptions"
-              placeholder="结束日期"
+        :disabled="isForever"
+        v-model="endTime"
+        :picker-options="endOptions"
+        placeholder="结束日期"
       />
-      <el-switch v-model="isForever" v-if="forever"
-                 active-text="永久有效"
-                 inactive-text=""/>
+      <el-switch
+        v-model="isForever"
+        v-if="forever"
+        active-text="永久有效"
+        inactive-text=""
+      />
     </template>
-    <DescBtn :text="data.description"/>
+    <DescBtn :text="data.description" />
   </div>
 </template>
 <script>
-  import dayjs from "dayjs";
-  import Common from "./Common";
+import dayjs from "dayjs";
+import Common from "./Common";
 
-  const fmt = d => (d ? dayjs(d).format("YYYY-MM-DD") : "");
+const fmt = d => (d ? dayjs(d).format("YYYY-MM-DD") : "");
 
-  function getStartLimit(min, end, max) {
+function getStartLimit(min, end, max) {
+  return {
+    minDate: min,
+    maxDate: end
+      ? dayjs(end).isAfter(dayjs(max))
+        ? max
+        : dayjs(end).toDate()
+      : max
+  };
+}
+
+function getEndLimit(start, max) {
+  return {
+    minDate: start ? dayjs(start).toDate() : dayjs().toDate(),
+    maxDate: max
+  };
+}
+
+function getLimitDate(type, start, end, min, max) {
+  return {
+    start: getStartLimit(min, end, max),
+    end: getEndLimit(start, max)
+  }[type];
+}
+
+export default {
+  name: "FDateRange",
+  components: {},
+  mixins: [Common],
+  props: {
+    value: {
+      type: String,
+      default: ","
+    },
+    min: {
+      type: Date,
+      default: () =>
+        dayjs()
+          .subtract(35, "year")
+          .toDate()
+    },
+    max: {
+      type: Date,
+      default: () =>
+        dayjs()
+          .add(100, "year")
+          .toDate()
+    },
+    forever: {
+      type: Boolean
+    }
+  },
+  filters: {
+    fmt
+  },
+  data() {
     return {
-      minDate: min,
-      maxDate: end ? dayjs(end).toDate() : max
+      isForever: false
     };
-  }
-
-  function getEndLimit(start, max) {
-    return {
-      minDate: start ? dayjs(start).toDate() : dayjs().toDate(),
-      maxDate: max
-    };
-  }
-
-  function getLimitDate(type, start, end, min, max) {
-    return {
-      start: getStartLimit(min, end, max),
-      end: getEndLimit(start, max)
-    }[type];
-  }
-
-  export default {
-    name: "FDateRange",
-    components: {},
-    mixins: [Common],
-    props: {
-      value: {
-        type: String,
-        default: ","
-      },
-      min: {
-        type: Date,
-        default: () => dayjs().subtract(35, "year").toDate()
-      },
-      max: {
-        type: Date,
-        default: () => dayjs().add(35, "year").toDate()
-      },
-      forever: {
-        type: Boolean,
-      }
-    },
-    filters: {
-      fmt
-    },
-    data() {
-      return {
-        isForever: false
-      };
-    },
-    created() {
-      this.isMobile && this.$services.datePick();
-    },
-    methods: {
-      async callTimePicker(type) {
-        const propName = `${type}Time`;
-        const { minDate, maxDate } = getLimitDate(
-          type,
-          this.startTime,
-          this.endTime,
-          this.min,
-          this.max
-        );
-        const result = await this.$services.datePick({
-          value: this[propName],
-          data: this.data,
-          minDate,
-          maxDate
-        });
-        if (result.success) {
-          console.log(result.value, propName);
-          this[propName] = result.value;
-        }
-      }
-    },
-    computed: {
-      // isForever: {
-      //   get() {
-      //     return dayjs("9999-12-31").isSame(this.endTime, "date");
-      //   },
-      //   set(v) {
-      //     if (v) {
-      //       this.endDisabled = true;
-      //       this.endTime = "9999-12-31";
-      //     } else {
-      //     }
-      //   }
-      // },
-      startTime: {
-        get() {
-          if (!this.value) return "";
-          const [s, e] = this.value.split(",");
-          if (!s) return "";
-          return dayjs(s).toDate();
-        },
-        set(v) {
-          const [s, e] = this.value.split(",");
-          this.$emit("input", `${fmt(v)},${e || ""}`);
-        }
-      },
-      endTime: {
-        get() {
-          if (!this.value) return "";
-          const [s, e] = this.value.split(",");
-          if (!e) return "";
-          return dayjs(e).toDate();
-        },
-        set(v) {
-          const [s, e] = this.value.split(",");
-          this.$emit("input", `${s || ""},${fmt(v)}`);
-        }
-      },
-
-      startOptions() {
-        return {
-          disabledDate: date => {
-            return (
-              dayjs(date).isAfter(dayjs(this.endTime)) ||
-              dayjs(date).isBefore(this.min)
-            );
-          }
-        };
-      },
-      endOptions() {
-        return {
-          disabledDate: date => {
-            return (
-              dayjs(date).isBefore(dayjs(this.startTime)) ||
-              dayjs(date).isBefore(this.min) ||
-              dayjs(date).isAfter(this.max)
-            );
-          }
-        };
-      }
-    },
-    watch: {
-      endTime: {
-        handler(now) {
-          console.log(now);
-          this.isForever = dayjs("9999-12-31").isSame(now, "date");
-        }, immediate: true
-      },
-      isForever(now) {
-        if (now) {
-          this.endTime = "9999-12-31";
-        }
-        else {
-          if (!dayjs(this.endTime).isSame(dayjs("9999-12-31"), "date")) return;
-          this.endTime = this.max;
-        }
+  },
+  created() {
+    this.isMobile && this.$services.datePick();
+  },
+  methods: {
+    async callTimePicker(type) {
+      if (type === "end" && this.isForever) return;
+      const propName = `${type}Time`;
+      const { minDate, maxDate } = getLimitDate(
+        type,
+        this.startTime,
+        this.endTime,
+        this.min,
+        this.max
+      );
+      const result = await this.$services.datePick({
+        value: this[propName],
+        data: this.data,
+        minDate,
+        maxDate
+      });
+      if (result.success) {
+        console.log(result.value, propName);
+        this[propName] = result.value;
       }
     }
-  };
+  },
+  computed: {
+    // isForever: {
+    //   get() {
+    //     return dayjs("9999-12-31").isSame(this.endTime, "date");
+    //   },
+    //   set(v) {
+    //     if (v) {
+    //       this.endDisabled = true;
+    //       this.endTime = "9999-12-31";
+    //     } else {
+    //     }
+    //   }
+    // },
+    startTime: {
+      get() {
+        if (!this.value) return "";
+        const [s, e] = this.value.split(",");
+        if (!s) return "";
+        return dayjs(s).toDate();
+      },
+      set(v) {
+        const [s, e] = this.value.split(",");
+        this.$emit("input", `${fmt(v)},${e || ""}`);
+      }
+    },
+    endTime: {
+      get() {
+        if (!this.value) return "";
+        const [s, e] = this.value.split(",");
+        if (!e) return "";
+        return dayjs(e).toDate();
+      },
+      set(v) {
+        const [s, e] = this.value.split(",");
+        this.$emit("input", `${s || ""},${fmt(v)}`);
+      }
+    },
+
+    startOptions() {
+      const { minDate, maxDate } = getLimitDate(
+        "start",
+        this.startTime,
+        this.endTime,
+        this.min,
+        this.max
+      );
+      return {
+        disabledDate: date => {
+          return (
+            dayjs(date).isAfter(dayjs(maxDate)) ||
+            dayjs(date).isBefore(dayjs(minDate))
+          );
+        }
+      };
+    },
+    endOptions() {
+      return {
+        disabledDate: date => {
+          return (
+            dayjs(date).isBefore(dayjs(this.startTime)) ||
+            dayjs(date).isBefore(this.min) ||
+            dayjs(date).isAfter(this.max)
+          );
+        }
+      };
+    }
+  },
+  watch: {
+    endTime: {
+      handler(now) {
+        console.log(now);
+        this.isForever = dayjs("9999-12-31").isSame(now, "date");
+      },
+      immediate: true
+    },
+    isForever(now) {
+      if (now) {
+        this.endTime = "9999-12-31";
+      } else {
+        if (!dayjs(this.endTime).isSame(dayjs("9999-12-31"), "date")) return;
+        this.endTime = this.max;
+      }
+    }
+  }
+};
 </script>
 
 <style lang="less" rel="stylesheet/less">
-  .f-date-range {
-    display: inline-block;
-    width: 100%;
+.f-date-range {
+  display: inline-block;
+  width: 100%;
 
-    .el-input {
-      width: 140px;
-    }
+  .el-input {
+    width: 140px;
+  }
 
+  .pick-holder {
+    display: inline-flex;
+    width: calc(100% - 30px);
 
-    .pick-holder {
-      display: inline-flex;
-      width: calc(100% - 30px);
-
-      &.forever {
-        display: block;
-
-        .time-display {
-          display: block;
-        }
-      }
-
-      &:after {
-        z-index: -1;
-      }
+    &.forever {
+      display: block;
 
       .time-display {
-        padding: 0 15px;
-        border: 1px solid #dcdfe6;
-        border-radius: 4px;
-        color: #606266;
-        max-width: 220px;
-        flex-basis: 100px;
-        flex-grow: 1;
-        flex-shrink: 1;
-        touch-action: none;
-        white-space: nowrap;
-
-        &.disabled {
-          background-color: #efefef;
-          color: #999;
-        }
-      }
-
-      .placeholder {
-        color: #ccc;
+        display: block;
       }
     }
+
+    &:after {
+      z-index: -1;
+    }
+
+    .time-display {
+      padding: 0 15px;
+      border: 1px solid #dcdfe6;
+      border-radius: 4px;
+      color: #606266;
+      max-width: 220px;
+      flex-basis: 100px;
+      flex-grow: 1;
+      flex-shrink: 1;
+      touch-action: none;
+      white-space: nowrap;
+
+      &.disabled {
+        background-color: #efefef;
+        color: #999;
+      }
+    }
+
+    .placeholder {
+      color: #ccc;
+    }
   }
+}
 </style>
