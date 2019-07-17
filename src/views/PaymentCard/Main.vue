c635612c1c581543954a8a12aa2138374fb26222
谢腾远 <redbuck@126.com>
2019-07-17 09:05:31 +0800

    refect: 去掉多余的按钮

--- src/views/PaymentCard/Main.vue
+++ src/views/PaymentCard/Main.vue
@@ -14,93 +14,89 @@
       <Drag :data="list" @drag-over="dragDone" ref="drag">
         <transition-group tag="div" :name="transitionName">
           <ChanelItem
-                  @up="itemUp"
-                  @down="itemDown"
-                  @top="itemTop"
-                  @resize="resize"
-                  @toggle="toggleItem"
-                  @confirm="confirmParams"
-                  v-for="(item, index) in list"
-                  :index="index"
-                  :key="item.key"
-                  :total="list.length"
-                  :data="item"
-                  :map="typeMap"
+            @up="itemUp"
+            @down="itemDown"
+            @top="itemTop"
+            @resize="resize"
+            @toggle="toggleItem"
+            @confirm="confirmParams"
+            v-for="(item, index) in list"
+            :index="index"
+            :key="item.key"
+            :total="list.length"
+            :data="item"
+            :map="typeMap"
           />
         </transition-group>
       </Drag>
     </section>
-    <footer class="footer" slot="foot">
-      <el-button type="primary">确定</el-button>
-    </footer>
   </Container>
 </template>
 
 <script>
-  import Container from "@/components/Container";
-  import { Alert } from "element-ui";
-  import Common from "./Common";
-  import ChanelItem from "./children/ChanelItem";
+import Container from "@/components/Container";
+import { Alert } from "element-ui";
+import Common from "./Common";
+import ChanelItem from "./children/ChanelItem";
 
-  export default {
-    name: "PaymentCard",
-    mixins: [Common],
-    components: { ChanelItem, ElAlert: Alert, Container },
-    data() {
-      return {};
-    },
-    created() {
-    },
-    methods: {},
-    computed: {}
-  };
+export default {
+  name: "PaymentCard",
+  mixins: [Common],
+  components: { ChanelItem, ElAlert: Alert, Container },
+  data() {
+    return {};
+  },
+  created() {},
+  methods: {},
+  computed: {}
+};
 </script>
 
 <style lang="less" rel="stylesheet/less">
-  .payment-card {
-    .content {
-      border: 1px solid #f0f2f5;
-      border-radius: 8px;
-      min-height: 500px;
-      padding: 30px 0;
-      overflow: hidden;
-    }
+.payment-card {
+  .content {
+    border: 1px solid #f0f2f5;
+    border-radius: 8px;
+    min-height: 500px;
+    padding: 30px 0;
+    overflow: hidden;
+  }
 
-    .content {
-      background-color: #f9f9f9;
-      padding: 0;
+  .content {
+    background-color: #f9f9f9;
+    padding: 0;
 
-      .title {
-        height: 48px;
-        background-color: #f5f7fa;
-        border-bottom: 1px solid #f2f4f6;
+    .title {
+      height: 48px;
+      background-color: #f5f7fa;
+      border-bottom: 1px solid #f2f4f6;
 
-        .pay-cell {
-          line-height: 48px;
-          font-weight: bold;
-          text-align: center;
-        }
-        .pay-index{
-          text-align: left;
-        }
+      .pay-cell {
+        line-height: 48px;
+        font-weight: bold;
+        text-align: center;
       }
-    }
-
-    .pay-cell {
-    }
-
-    .footer {
-      padding: 40px;
-      text-align: center;
-
-      .el-button {
-        height: 42px;
-        width: 240px;
+      .pay-index {
+        text-align: left;
       }
-    }
-
-    .flip-move {
-      transition: 0.3s;
     }
   }
+
+  .pay-cell {
+  }
+
+  .footer {
+    padding: 40px;
+    text-align: center;
+
+    .el-button {
+      height: 42px;
+      width: 240px;
+    }
+  }
+
+  .flip-move {
+    transition: 0.3s;
+  }
+}
 </style>
